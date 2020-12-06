80a6ac1bef6e606e8356a6fa70b6ebaf3a90565a
rachmadaniHaryono <foreturiga@gmail.com>
2020-12-06 21:39:34 +0800

    chg: dev: remove old main test

--- test.py
+++ test.py
@@ -169,54 +169,6 @@ def test_main(run_tuple, test, request):
     LOGGER.info("[+] Testing complete!")
 
 
-def main():
-    c = twint.Config()
-    c.Username = "verified"
-    c.Limit = 20
-    c.Store_object = True
-
-    # Separate objects are necessary.
-
-    f = twint.Config()
-    f.Username = "verified"
-    f.Limit = 20
-    f.Store_object = True
-    f.User_full = True
-
-    runs = [
-        twint.run.Profile,  # this doesn't
-        twint.run.Search,  # this works
-        twint.run.Following,
-        twint.run.Followers,
-        twint.run.Favorites,
-    ]
-
-    tests = [test_reg, test_json, test_csv, test_db]
-
-    # Something breaks if we don't split these up
-
-    for run in runs[:3]:
-        if run == twint.run.Search:
-            c.Since = "2012-1-1 20:30:22"
-            c.Until = "2017-1-1"
-        else:
-            c.Since = ""
-            c.Until = ""
-
-        for test in tests:
-            test(c, run)
-
-    for run in runs[3:]:
-        for test in tests:
-            test(f, run)
-
-    files = ["test_twint.db", "test_twint.json", "test_twint.csv"]
-    for _file in files:
-        os.remove(_file)
-
-    print("[+] Testing complete!")
-
-
 @pytest.mark.vcr
 def test_db_retweet(tmp_path):
     co = twint.Config()
